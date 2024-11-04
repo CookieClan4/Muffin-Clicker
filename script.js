73bb30296974231ea34c1672199d1debd59e3e4d
CookieClan4 <christian.cooke07@icloud.com>
2024-11-04 16:06:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,57 @@
-var muffins = 0;
-var clickOne = 0;
-var clickTwo = 0;
-var passiveOne = 0;
-var passiveTwo = 0;
-var boosterOne = 0;
-var boosterTwo = 0;
-var boosterThree = 0;
+if (window.localStorage.muffins) {
+  var muffins = parseFloat(window.localStorage.muffins);
+} else {
+  var muffins = 0;
+}
+if (window.localStorage.clickOne) {
+  var clickOne = parseInt(window.localStorage.clickOne);
+} else {
+  var clickOne = 0;
+}
+if (window.localStorage.clickTwo) {
+  var clickTwo = parseInt(window.localStorage.clickTwo);
+} else {
+  var clickTwo = 0;
+}
+if (window.localStorage.passiveOne) {
+  var passiveOne = parseInt(window.localStorage.passiveOne);
+} else {
+  var passiveOne = 0;
+}
+if (window.localStorage.passiveTwo) {
+  var passiveTwo = parseInt(window.localStorage.passiveTwo);
+} else {
+  var passiveTwo = 0;
+}
+if (window.localStorage.boosterOne) {
+  var boosterOne = parseInt(window.localStorage.boosterOne);
+  document.getElementById("upgradeOne").innerText = "BOUGHT";
+} else {
+  var boosterOne = 0;
+}
+if (window.localStorage.boosterTwo) {
+  var boosterTwo = parseInt(window.localStorage.boosterTwo);
+  document.getElementById("upgradeTwo").innerText = "BOUGHT";
+} else {
+  var boosterTwo = 0;
+}
+if (window.localStorage.boosterThree) {
+  var boosterThree = parseInt(window.localStorage.boosterThree);
+  document.getElementById("upgradeThree").innerText = "BOUGHT";
+} else {
+  var boosterThree = 0;
+}
 
 function handleClick() {
   muffins = muffins + (clickOne + (clickTwo * 6)) + (1 + (0.1 * boosterOne));
   updateDisplay();
+  window.localStorage.setItem("muffins", muffins);
 }
 
 function passive() {
   muffins = muffins + ((passiveOne + (passiveTwo * 3)) / 10 * (1 + (0.1 * boosterTwo)));
   updateDisplay();
+  window.localStorage.setItem("muffins", muffins);
 }
 setInterval(passive, 100)
 
@@ -23,6 +60,8 @@ function buyClickOne() {
     muffins = muffins - Math.floor((1.2 ** clickOne) * 15 * (1 - (0.1 * boosterThree)));
     clickOne = clickOne + 1;
     updateDisplay();
+    window.localStorage.setItem("clickOne", clickOne);
+    window.localStorage.setItem("muffins", muffins);
   }
 }
 
@@ -31,6 +70,8 @@ function buyClickTwo() {
     muffins = muffins - Math.floor((1.2 ** clickTwo) * 500 * (1 - (0.1 * boosterThree)));
     clickTwo = clickTwo + 1;
     updateDisplay();
+    window.localStorage.setItem("clickTwo", clickTwo);
+    window.localStorage.setItem("muffins", muffins);
   }
 }
 
@@ -39,6 +80,8 @@ function buyPassiveOne() {
     muffins = muffins - Math.floor((1.2 ** passiveOne) * 30 * (1 - (0.1 * boosterThree)));
     passiveOne = passiveOne + 1;
     updateDisplay();
+    window.localStorage.setItem("passiveOne", passiveOne);
+    window.localStorage.setItem("muffins", muffins);
   }
 }
 
@@ -47,6 +90,8 @@ function buyPassiveTwo() {
     muffins = muffins - Math.floor((1.2 ** passiveTwo) * 350 * (1 - (0.1 * boosterThree)));
     passiveTwo = passiveTwo + 1;
     updateDisplay();
+    window.localStorage.setItem("passiveTwo", passiveTwo);
+    window.localStorage.setItem("muffins", muffins);
   }
 }
 
@@ -57,6 +102,8 @@ function buyUpgradeOne() {
     boosterOne = 1;
     document.getElementById("upgradeOne").innerText = "BOUGHT";
     updateDisplay();
+    window.localStorage.setItem("boosterOne", boosterOne);
+    window.localStorage.setItem("muffins", muffins);
     }
   }
 }
@@ -68,6 +115,8 @@ function buyUpgradeTwo() {
     boosterTwo = 1;
     document.getElementById("upgradeTwo").innerText = "BOUGHT";
     updateDisplay();
+    window.localStorage.setItem("boosterTwo", boosterTwo);
+    window.localStorage.setItem("muffins", muffins);
     }
   }
 }
@@ -79,6 +128,8 @@ function buyUpgradeThree() {
     boosterThree = 1;
     document.getElementById("upgradeThree").innerText = "BOUGHT";
     updateDisplay();
+    window.localStorage.setItem("boosterThree", boosterThree);
+    window.localStorage.setItem("muffins", muffins);
     }
   }
 }
@@ -91,9 +142,27 @@ function updateDisplay() {
   document.getElementById("clickUpgradeTwo").innerText = Math.floor((1.2 ** clickTwo) * 500 * (1 - (0.1 * boosterThree))) + " Muffins";
   document.getElementById("passiveUpgradeOne").innerText = Math.floor((1.2 ** passiveOne) * 30 * (1 - (0.1 * boosterThree))) + " Muffins";
   document.getElementById("passiveUpgradeTwo").innerText =  Math.floor((1.2 ** passiveTwo) * 350 * (1 - (0.1 * boosterThree))) + " Muffins";
-  
 }
 
+function reset() {
+  localStorage.removeItem("muffins");
+  muffins = 0;
+  localStorage.removeItem("clickOne");
+  clickOne = 0;
+  localStorage.removeItem("clickTwo");
+  click = 0;
+  localStorage.removeItem("passiveOne");
+  passiveOne = 0;
+  localStorage.removeItem("passiveTwo");
+  passiveTwo = 0;
+  localStorage.removeItem("boosterOne");
+  boosterOne = 0;
+  localStorage.removeItem("boosterTwo");
+  boosterTwo = 0;
+  localStorage.removeItem("boosterThree");
+  boosterThree = 0;
+  location.reload();
+}
 
 document.getElementById("muffinButton").addEventListener("click", handleClick);
 document.getElementById("clickUpgradeOne").addEventListener("click", buyClickOne);
@@ -103,5 +172,4 @@ document.getElementById("passiveUpgradeTwo").addEventListener("click", buyPassiv
 document.getElementById("upgradeOne").addEventListener("click", buyUpgradeOne);
 document.getElementById("upgradeTwo").addEventListener("click", buyUpgradeTwo);
 document.getElementById("upgradeThree").addEventListener("click", buyUpgradeThree);
-
-
+document.getElementById("reset").addEventListener("click", reset);
